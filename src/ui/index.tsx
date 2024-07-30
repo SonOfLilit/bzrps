2bb7774f6c4ff56435f4bf3b808fe0845e4ec8b4
Aur Saraf <aur@sarafconsulting.com>
2024-07-31 00:17:46 +0300

    bug in layout

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { render } from '@boardzilla/core';
-import { default as setup } from '../game/index.js';
+import React from "react";
+import { Piece, Space, render } from "@boardzilla/core";
+import { Card, default as setup } from "../game/index.js";
 
-import './style.scss';
+import "./style.scss";
 
 render(setup, {
-  settings: {
+  settings: {},
+  layout: (game) => {
+    game.all(Card).appearance({ aspectRatio: 3 / 4 });
+    game.all(Space, { name: "hand" }).layout(Piece, {
+      rows: { max: 1 },
+      gap: { x: 3, y: 0 },
+      haphazardly: 0.2,
+    });
+    game.all(Space, { name: "sequence" }).layout(Piece, {
+      rows: { max: 1 },
+      gap: { x: 3, y: 0 },
+      haphazardly: 0.2,
+    });
+    game.all(Space, { name: "stack" }).layout(Piece, {
+      columns: { max: 1 },
+      gap: { x: 0, y: 3 },
+    });
+    game.all(Space, { name: "slots" }).layout(Piece, {
+      rows: { max: 1 },
+      gap: { x: 3, y: 0 },
+    });
   },
-  layout: game => {
-  }
 });
